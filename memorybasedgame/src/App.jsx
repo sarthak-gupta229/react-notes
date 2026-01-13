30d831f0becfb01d220bd4b441d661ccf955b72c
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-01-14 01:35:27 +0530

    memo

---
+++ memorybasedgame/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import MemoryGames from "./components/MemoryGames";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MemoryGames/>
+    </>
+  );
+}
+
+export default App;
