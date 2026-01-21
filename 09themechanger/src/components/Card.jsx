fdcd42476e97e7e8cb14b03bf53bcf120bec5df0
“sarthak-gupta229Git config --global user.email “guptasarthak229@gmail.com <guptasarthak229@gmail.com>
2026-01-22 01:38:52 +0530

    themeToggle using react/tailwind

--- 09themechanger/src/components/Card.jsx
+++ 09themechanger/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://rukmini1.flixcart.com/image/1500/1500/ku8pbbk0/smartwatch/a/y/1/ios-mkn63hn-a-apple-yes-original-imag7eqbsnzggwsm.jpeg?q=70" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
